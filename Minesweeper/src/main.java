e04919e95e0f4dbf29548a3e944687e0c0cb8142
IbraheemF <141586321+IbraheemF@users.noreply.github.com>
2023-12-20 13:54:56 -0600

    Update main.java

--- Minesweeper/src/main.java
+++ Minesweeper/src/main.java
@@ -2,18 +2,6 @@
 import java.util.Random;
 import java.util.Scanner;
 
-/*
- * Provided in this class is the neccessary code to get started with your game's implementation
- * You will find a while loop that should take your minefield's gameOver() method as its conditional
- * Then you will prompt the user with input and manipulate the data as before in project 2
- * 
- * Things to Note:
- * 1. Think back to project 1 when we asked our user to give a shape. In this project we will be asking the user to provide a mode. Then create a minefield accordingly
- * 2. You must implement a way to check if we are playing in debug mode or not.
- * 3. When working inside your while loop think about what happens each turn. We get input, user our methods, check their return values. repeat.
- * 4. Once while loop is complete figure out how to determine if the user won or lost. Print appropriate statement.
- */
-
 // written by Ibraheem Fannoun
 public class main{
     public static void main(String[] args) {
@@ -49,7 +37,6 @@ public class main{
         System.out.println(gameField);
         int startX = Integer.parseInt(sc.nextLine());
         int startY = Integer.parseInt(sc.nextLine());
-        //Minefield gameField = new Minefield(r,c,f); // intialize cell  array
         gameField.createMines(startX,startY,f); // creates mines on the board
         gameField.evaluateField(); // properly changes all cells status
         gameField.revealStartingArea(startX, startY); // reveals the status of the starting area the user chose
